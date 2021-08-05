fcb3594440c660b20a5bc5fe9201010b03bc6861
Martin Muller <damadden88@googlemail.com>
2021-08-05 07:06:14 +0200

    adjust info

--- src/upload-bucket-stack.ts
+++ src/upload-bucket-stack.ts
@@ -1,25 +1,21 @@
-// import * as lambda from '@aws-cdk/aws-lambda';
 import { S3EventSource } from '@aws-cdk/aws-lambda-event-sources';
 import * as lambdajs from '@aws-cdk/aws-lambda-nodejs';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
-import { CustomStack } from 'aws-cdk-staging-pipeline/lib/custom-stack';
 import * as statement from 'cdk-iam-floyd';
 
 export interface UploadBucketStackProps extends cdk.StackProps {
   readonly stage: string;
 }
 
-export class UploadBucketStack extends CustomStack {
+export class UploadBucketStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: UploadBucketStackProps) {
     super(scope, id, props);
 
-    // Create S3 Bucket
-    const bucket = new s3.Bucket(this, 'hacklab-videos', {
-      bucketName: 'hacklab-videos',
-      // removalPolicy: cdk.RemovalPolicy.DESTROY,
-      // autoDeleteObjects: true,
-      publicReadAccess: true,
+    const bucket = new s3.Bucket(this, 'my-videos', {
+      bucketName: 'my-videos',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
     const lambda = new lambdajs.NodejsFunction(this, 'upload-trigger', {
@@ -37,7 +33,6 @@ export class UploadBucketStack extends CustomStack {
 
     lambda.addEventSource(new S3EventSource(bucket, {
       events: [s3.EventType.OBJECT_CREATED],
-      // filters: [{ prefix: 'subdir/' }], // optional
     }));
   }
 }
